04e3efa81ddebed342d83a06dce83571ed1452dd
Sikvoyo <suruhakken@gmail.com>
2022-07-12 16:19:33 +0300

    abt me

--- sikvoyo/src/components/AboutMe/AboutMe.jsx
+++ sikvoyo/src/components/AboutMe/AboutMe.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import pfp from '../../img/pfp.jpg';
 import heart from '../../img/heart.png';
 import './AboutMe.css';
+import Fade from 'react-reveal/Fade';
 import { DEFAULT_BG_TEXT } from "../../consts";
 
 const AboutMe = (props) => {
@@ -42,14 +43,16 @@ const AboutMe = (props) => {
 
    return(
       <section id="aboutme">
-         <div onMouseOver={() => {hoverChange("Asuka")}} onMouseLeave={mouseLeave}>
-            <img src={pfp} alt="" id="pfp" />
-         </div>
-         <h1 className="purple" id="hi" onMouseOver={() => {hoverChange("My real name is Maxim")}} onMouseLeave={mouseLeave}>Hi! I am sikvoyø</h1>
-         <h2 className="purple" onMouseOver={() => {hoverChange("++++++++")}} onMouseLeave={mouseLeave}>{descriptions.join(" + ")}</h2>
-         <div className="centerHeart" onMouseOver={() => {hoverChange("<3")}} onMouseLeave={mouseLeave}>
-            <img src={heart} alt="" className="heart" />
-         </div>
+         <Fade top duration={750} distance={"10%"}>
+            <div onMouseOver={() => {hoverChange("Asuka")}} onMouseLeave={mouseLeave} id="pfpcenter">
+               <img src={pfp} alt="" id="pfp" />
+            </div>
+            <h1 className="purple" id="hi" onMouseOver={() => {hoverChange("My real name is Maxim")}} onMouseLeave={mouseLeave}>Hi! I am sikvoyø</h1>
+            <h2 className="purple" onMouseOver={() => {hoverChange("++++++++")}} onMouseLeave={mouseLeave}>{descriptions.join(" + ")}</h2>
+            <div className="centerHeart" onMouseOver={() => {hoverChange("<3")}} onMouseLeave={mouseLeave} style={{animation: 'Heartbeat 1s infinite'}}>
+               <img src={heart} alt="" className="heart" />
+            </div>
+         </Fade>
       </section>
    );
 }
